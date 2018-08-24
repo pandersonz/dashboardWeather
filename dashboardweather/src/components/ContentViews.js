33d3c636cdf069e2b716e03f9b133c78055678b0
paul <p.anderson2390@gmail.com>
2018-08-24 18:22:21 -0400

    structure: structure of graph

--- dashboardweather/src/components/ContentViews.js
+++ dashboardweather/src/components/ContentViews.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import './ContentViews.css';
 import {Bar,Line,Pie} from 'react-chartjs-2';
-let data= {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
+let dataDia= {
+  labels: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00","13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"],
+  datasets: [{
+  label: "My First dataset",
+  backgroundColor: 'rgb(255, 99, 132)',
+  borderColor: 'rgb(255, 99, 132)',
+  data: [0, 10, 5, 2, 20, 30, 45, 10, 5, 2, 20, 30, 45, 10, 5, 2, 20, 30, 45, 5, 2, 20, 30, 45],
+  }]
+}
+let dataSemana= {
+  labels: ["lunes", "martes", "miercoles", "jueves", "viernes","sabado","domingo"],
   datasets: [{
   label: "My First dataset",
   backgroundColor: 'rgb(255, 99, 132)',
@@ -10,22 +19,13 @@ let data= {
   data: [0, 10, 5, 2, 20, 30, 45],
   }]
 }
-let data2= {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
+let dataMes= {
+  labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
   datasets: [{
   label: "My First dataset",
   backgroundColor: 'rgb(255, 99, 132)',
   borderColor: 'rgb(255, 99, 132)',
-  data: [0, 10, 5, 2, 20, 30, 45],
-  }]
-}
-let data3= {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [{
-  label: "My First dataset",
-  backgroundColor: 'rgb(255, 99, 132)',
-  borderColor: 'rgb(255, 99, 132)',
-  data: [0, 10, 5, 2, 20, 30, 45],
+  data: [0, 10, 5, 2, 20, 30, 45, 10, 5, 2, 20, 30, 45,0, 10, 5, 2, 20, 30, 45, 20, 30, 45,0, 10, 5, 2, 20, 30, 45],
   }]
 }
 let data4= {
@@ -43,12 +43,20 @@ class ContentViews extends Component {
       <div className="contentViews">
         <div className="rowContent">
           <div className="cardChart" >
-            <Line data={data} width='30vw' height='20vh' />
+          <h4>Historico de tiempo / dia</h4>
+            <Bar data={dataDia} width='30vw' height='10vh' />
           </div>
           <div className="cardChart" >
-            <Bar data={data2} width='30vw' height='20vh' />
+          <h4>Historico de tiempo Semanal</h4>
+            <Bar data={dataSemana} width='30vw' height='10vh' />
           </div>
         </div>
+        <div className="rowContent">
+        <div className="cardChartComplete" >
+          <h4>Historico de tiempo Semanal</h4>
+            <Bar data={dataMes} width='30vw' height='5vh' />
+          </div>
+        </div>  
       </div>
     );
   }
--- dashboardweather/src/components/Header.js
+++ dashboardweather/src/components/Header.js
@@ -6,7 +6,17 @@ class Header extends Component {
     return (
       <div className="header">
         <div className='logo'></div>
-        <div className='info'></div>
+        <div className='info'><select>
+          <option value='Santa Cruz'>Santa Cruz</option>
+          <option value='La Paz'>La Paz</option>
+          <option value='Cochabamba'>Cochabamba</option>
+          <option value='Trinidad'>Trinidad</option>
+          <option value='Potosi'>Potosi</option>
+          <option value='Cobija'>Cobija</option>
+          <option value='Tarija'>Tarija</option>
+          <option value='Sucre'>Sucre</option>
+          <option value='Oruro'>Oruro</option>
+        </select></div>
       </div>
     );
   }
