33b5c889e4576bbc041a96ca74110b8268759e7d
paul <p.anderson2390@gmail.com>
2018-08-24 17:23:05 -0400

    CSS: created css and structure of dasboard

--- dashboardweather/src/components/ContentViews.js
+++ dashboardweather/src/components/ContentViews.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
 import './ContentViews.css';
-
-
+import {Bar,Line,Pie} from 'react-chartjs-2';
+let data= {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [{
+  label: "My First dataset",
+  backgroundColor: 'rgb(255, 99, 132)',
+  borderColor: 'rgb(255, 99, 132)',
+  data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+}
+let data2= {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [{
+  label: "My First dataset",
+  backgroundColor: 'rgb(255, 99, 132)',
+  borderColor: 'rgb(255, 99, 132)',
+  data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+}
+let data3= {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [{
+  label: "My First dataset",
+  backgroundColor: 'rgb(255, 99, 132)',
+  borderColor: 'rgb(255, 99, 132)',
+  data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+}
+let data4= {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [{
+  label: "My First dataset",
+  backgroundColor: 'rgb(255, 99, 132)',
+  borderColor: 'rgb(255, 99, 132)',
+  data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+}
 class ContentViews extends Component {
   render() {
     return (
       <div className="contentViews">
-        
+        <div className="rowContent">
+          <div className="cardChart" >
+            <Line data={data} width='30vw' height='20vh' />
+          </div>
+          <div className="cardChart" >
+            <Bar data={data2} width='30vw' height='20vh' />
+          </div>
+        </div>
       </div>
     );
   }
--- dashboardweather/src/dashboard.js
+++ dashboardweather/src/dashboard.js
@@ -1,73 +1,21 @@
 import React, { Component } from 'react';
 import Menu from './components/Menu';
-import contentViews from './components/ContentViews';
+import ContentViews from './components/ContentViews';
 import Header from './components/Header';
-import { Bar, Line } from 'react-chartjs-2';
 
-const cardChartData1 = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      backgroundColor: brandPrimary,
-      borderColor: 'rgba(255,255,255,.55)',
-      data: [65, 59, 84, 84, 51, 55, 40],
-    },
-  ],
-};
+import './dashboard.css'
 
-const cardChartOpts1 = {
-  tooltips: {
-    enabled: false,
-    custom: CustomTooltips
-  },
-  maintainAspectRatio: false,
-  legend: {
-    display: false,
-  },
-  scales: {
-    xAxes: [
-      {
-        gridLines: {
-          color: 'transparent',
-          zeroLineColor: 'transparent',
-        },
-        ticks: {
-          fontSize: 2,
-          fontColor: 'transparent',
-        },
 
-      }],
-    yAxes: [
-      {
-        display: false,
-        ticks: {
-          display: false,
-          min: Math.min.apply(Math, cardChartData1.datasets[0].data) - 5,
-          max: Math.max.apply(Math, cardChartData1.datasets[0].data) + 5,
-        },
-      }],
-  },
-  elements: {
-    line: {
-      borderWidth: 1,
-    },
-    point: {
-      radius: 4,
-      hitRadius: 10,
-      hoverRadius: 4,
-    },
-  }
-}
 
 class Dashboard extends Component {
+  
   render() {
     return (
       <div className="dashboard">
         <Header/>
-        <div>
-        <Menu/>
-        <contentViews/>
+        <div className="content">
+          <Menu/>
+          <ContentViews/>
         </div>
       </div>
     );
