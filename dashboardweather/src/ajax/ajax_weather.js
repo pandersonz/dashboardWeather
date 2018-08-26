059bfa2b3180524718965133e2d5a9c267fab823
paul <p.anderson2390@gmail.com>
2018-08-25 23:55:08 -0400

    code: added in ajax_weather obtener weather

--- dashboardweather/src/ajax/ajax_weather.js
+++ dashboardweather/src/ajax/ajax_weather.js
@@ -1,7 +1,7 @@
 
 import $ from 'jquery';
 
-export function mostrarKardexConductor(that,city){
+export function obtenerWeather(that,city){
     $.ajax({
         url: 'http://127.0.0.1/datos',
         type: "POST",
@@ -16,10 +16,19 @@ export function mostrarKardexConductor(that,city){
             result= array['data'];
             var url = array['url']
             console.log(url);
-                   
+           
                 let vector={};
-                vector['city']=result[0].ci;
-                
+                vector['name']=result[0].name;
+                vector['description']=result[0].descriptionWeather;
+                vector['currenttemp']=result[0].currentTemp;
+                vector['mintemp']=result[0].minTemp;
+                vector['maxtemp']=result[0].maxTemp;
+                vector['humidity']=result[0].humidity;
+                vector['pressure']=result[0].pressure;
+                vector['speedWind']=result[0].speedWind;
+                vector['degreeWind']=result[0].degreeWind;
+                vector['rain']=result[0].rain;
+                vector['dateWeather']=result[0].dateWeather;
 
                 that.setState({objWeather:vector}) ;  
                 console.log(vector);      
